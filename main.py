097484d9ac86a34c763f4a8c7cc4c5a5752d6c26
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-08-22 15:32:05 -0300

    Programa

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 import pandas as pd
 
 
-def contarAlunos(doc):
-    total = doc['RA'].count()
-    print('Total de Alunos Registrados: {}'.format(total))
+class Alunos:
+    @staticmethod
+    def TotalAlunos(doc):
+        # Conta o número de registros.
+        total = doc['RA'].count()
+        print('Total de Alunos Registrados: {}'.format(total))
+
+    @staticmethod
+    def MediaAlunos(doc):
+        print('=' * 45)
+        print('Média final de Todos os Alunos:')
+        # Cria a coluna 'Média'.
+        doc['Média'] = round((doc['P1'] + doc['P2'] + doc['Trabalho']) / 3, 1)
+        # Retorna o nome do aluno e sua média correspondente.
+        print(doc[['Nome', 'Média']])
+
+    @staticmethod
+    def MediaSala(doc):
+        print('=' * 45)
+        # Cria um Contador.
+        contador = doc['Média'].count()
+        # Soma todas as Médias individuais.
+        soma = sum(doc['Média'])
+        # Cria a media total.
+        media = round(soma / contador, 1)
+        print('Média de Desempenho da Sala: {}'.format(media))
+
+    @staticmethod
+    def Recuperacao(doc):
+        print('=' * 45)
+        print('Alunos que precisarão fazer a P3:')
+        # Pega as médias que são menores que 6.0.
+        doc_med = doc[doc['Média'] < 6.0]
+        # Retorna o nome e a Média dos Alunos correspondente.
+        print(doc_med[['Nome', 'Média']])
+
+    def AbrirArquivo(self):
+        try:
+            # Abre o arquivo CSV.
+            arq = pd.read_csv('nota_alunos.csv', sep=r'\s*,\s*', header=0, encoding='ascii', engine='python')
+            print('=' * 45)
+            print(arq)
+            print('=' * 45)
+            # Chama as funções
+            self.TotalAlunos(arq)
+            self.MediaAlunos(arq)
+            self.MediaSala(arq)
+            self.Recuperacao(arq)
+        except FileNotFoundError:
+            print('Erro: Arquivo não encontrado no Diretório.')
 
 
-def mostrarMedia(doc):
-    print('=' * 45)
-    print('Média final de Todos os Alunos:')
-    doc['Média'] = round((doc['P1'] + doc['P2'] + doc['Trabalho']) / 3, 1)
-    print(doc[['Nome', 'Média']])
-
-
-def mediaTotal(doc):
-    print('=' * 45)
-    contador = doc['Média'].count()
-    soma = sum(doc['Média'])
-    media = round(soma / contador, 1)
-    print('Média de Desempenho da Sala: {}'.format(media))
-
-
-def mostrarRecuperacao(doc):
-    print('=' * 45)
-    print('Alunos que precisarão fazer a P3:')
-    doc_med = doc[doc['Média'] <= 6.0]
-    print(doc_med[['Nome', 'Média']])
-
-
-try:
-    arq = pd.read_csv('nota_alunos.csv', sep=r'\s*,\s*', header=0, encoding='ascii', engine='python')
-    print('=' * 45)
-    print(arq)
-    print('=' * 45)
-    contarAlunos(arq)
-    mostrarMedia(arq)
-    mediaTotal(arq)
-    mostrarRecuperacao(arq)
-except FileNotFoundError:
-    print('Erro: Arquivo não encontrado no Diretório.')
+user = Alunos()
+user.AbrirArquivo()
